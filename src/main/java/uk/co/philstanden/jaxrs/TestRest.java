eae7fe7450afc7cb08aac4d118c203691ead3a17
523d6a9a4382ec6b600000fe <phil.standen@gmail.com>
2017-06-25 19:57:30 +0100

    Run and then reboot?

--- src/main/java/uk/co/philstanden/jaxrs/TestRest.java
+++ src/main/java/uk/co/philstanden/jaxrs/TestRest.java
@@ -15,6 +15,6 @@ public class TestRest {
 
     @GET
     public Map<String, String> test(){
-        return Collections.singletonMap("Hi", "pi");
+        return Collections.singletonMap("Hi", "cron");
     }
 }
